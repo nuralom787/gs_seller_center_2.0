911f792d73dc7020317b72a8166514fecac3eccb
nuralom787 <md9577630@gmail.com>
2025-03-07 16:51:55 +0600

    change localhost to live api

--- src/Components/Hooks/useAxiosSecure.jsx
+++ src/Components/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 // import { AuthContext } from "../Providers/AuthProvider";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://gs-dashboard-4864dwebapp.vercel.app'
 });
 
 const useAxiosSecure = () => {
